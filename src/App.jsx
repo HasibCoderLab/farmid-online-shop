1b50e0e5099941a3276f54a782a54a73eb911f96
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-11-03 20:09:07 +0600

    add hooks , function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect, useState } from "react";
+
 
 const App = () => {
+  // ============= Hooks , Function
+  const [orderPopup , setOrderPopup] = useState(false);
+  const handleOrderPopup = () =>{
+    
+setOrderPopup(!orderPopup)
+  }
+  useEffect(() =>{
+AOS.init({
+  offset:100,
+  duration:800,
+  easing:"ease-in-sing",
+  delay:100,
+});
+AOS.refresh();
+  },[]);
+  // ==================  =============
   return (
     <div>App</div>
   )
