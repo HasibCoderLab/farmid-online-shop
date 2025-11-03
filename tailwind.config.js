4905f9baadc15c3c8871e9283ad35c6b4e84de0c
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-11-03 19:57:52 +0600

    update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#4263eb",
+        secondary:"#63e6be"
+      },
+      container:{
+        center:true,
+        padding:{
+          DEFAULT:"1rem",
+          sm:"3rem"
+            
+          
+        }
+      }
+    },
   },
   plugins: [],
 }
