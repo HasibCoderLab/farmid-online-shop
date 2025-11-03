2544118f1f4a2c4286345f18854f6e4201f74f7d
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-11-03 21:04:31 +0600

    add img

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import BannerImg from "../../assets/4547829.jpg"
+import { GrSecure } from 'react-icons/gr'
+import { IoFastFood } from 'react-icons/io5'
+import { GiFoodTruck } from 'react-icons/gi'
+
+const Banner = () => {
+  return (
+    <div className='min-h-[550px] flex  justify-center items-center py-6 sm:py-0'>
+
+    </div>
+  )
+}
+
+export default Banner
