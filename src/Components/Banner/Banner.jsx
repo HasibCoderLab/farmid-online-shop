c7a230513ce98f1d05f6f82fb924b7426dd3a800
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-11-03 21:23:16 +0600

    update style

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,11 +6,21 @@ import { GiFoodTruck } from 'react-icons/gi'
 
 const Banner = () => {
     return (
-        <div className='min-h-[550px] flex  justify-center items-center py-6 sm:py-0'>
+        <div className='min-h-[550px] flex  justify-center items-center py-5 sm:py-0'>
             <div className='container'>
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 items-center">
                     <div data-aos="zoom-in">
                         <img className='max-w-[400px] h-[350px] w-full mx-auto object-cover' src={BannerImg} alt="BannerImg" />
+                    </div>
+                    <div className="flex flex-col justify-center gap-6  sm:pt-0">
+                        <h1 data-aos="fade-up" className="text-3xl sm:text-4xl font-bold"> Winter Sele Upto 50% off</h1>
+                        <p data-aos="fade-up" className="text-sm text-gray-500 tracking-wide leading-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id maxime sit accusantium earum at libero reprehenderit aut, maiores temporibus aspernatur repellendus facilis dolores mollitia? Molestiae suscipit sunt ullam perspiciatis soluta, dignissimos harum.</p>
+                    </div>
+                    <div className="flex  flex-col gap-4">
+                        <div data-aos="fade-up"  className="flex items-center gap-4">
+                            <GrSecure className='text-4xl h-12 w-12 shadow-sm p-4  rounded-full bg-violet-100 dark:bg-violet-400' />
+                            <p>Quality Product</p>
+                        </div>
                     </div>
                 </div>
             </div>
